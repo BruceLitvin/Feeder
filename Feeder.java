868b6facc8a19edc4667dde3f51295476be9c9f6
BruceLitvin <brucel15@nycstudents.net>
2025-09-19 12:01:22 -0400

    Added more stuff

--- Feeder.java
+++ Feeder.java
@@ -36,11 +36,15 @@ public class Feeder {
      */
     public int simulateManyDays(int numBirds, int numDays) {
         int days = 0;
-        while (numDays > 0) {
+        while (numDays > 0)
+        {
+            if (currentFood==0) return days;
             simulateOneDay(numBirds);
             numDays--;
+            days++;
+
         }
-        return 0;
+        return days;
 
     }
 }
--- Main.java
+++ Main.java
@@ -12,5 +12,9 @@ public class Main
         System.out.println("CurrentFood:"+ f.getCurrentFood());
         f= new Feeder(2400);
         System.out.println(f.simulateManyDays(10,4)+"days");
+        f=new Feeder(250);
+        System.out.println(f.simulateManyDays(10,5)+"days");
+        f=new Feeder(0);
+        System.out.println(f.simulateManyDays(5,10)+"days");
     }
 }
